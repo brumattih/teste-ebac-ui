1c90a221c18d03782a81704dcfd2acf12e5ea1b7
Henrique Brumatti <hrpreis@gmail.com>
2024-09-10 21:29:49 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('teste@testando.com')
+        cy.get('#password').type('12345678')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste-0999')
+    })
+})
