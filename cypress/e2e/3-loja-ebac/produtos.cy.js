ddae9ad8dce778cc0b50169a2e0cb34e3bb9d4b1
Henrique Brumatti <hrpreis@gmail.com>
2024-09-19 17:20:36 -0300

    adicionando page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,34 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page"
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     })
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Apollo Running Short')
-            .click()
+        produtosPage.buscarProdutoLista('Apollo Running Short')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    })
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    })
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Apollo Running Short')
+        cy.get('.product_title').should('contain', 'Apollo Running Short')
+    })
+
+    it.only('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Apollo Running Short')
+        cy.wait(500)
+        produtosPage.addProdutoCarrinho('34', 'Black', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Apollo Running Short” foram adicionados no seu carrinho.')
     })
 
 })
